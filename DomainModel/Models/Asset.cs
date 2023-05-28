95115363eb89e545850f13d6061a8604f94af6c5
firas-alalo <105157485+firas-alalo@users.noreply.github.com>
2023-05-28 18:56:55 +0200

    Create model for Asset with all properties

---
+++ DomainModel/Models/Asset.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
+
+namespace DomainModel.Models
+{
+    public class Asset
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Notes { get; set; }
+        public decimal Capacity { get; set; }
+        public decimal? Location_Longitude { get; set; }
+        public decimal? Location_Latitude { get; set; }
+        public DateOnly ContractStart { get; set; }
+        public DateOnly ContractEnd { get; set; }
+        public string ApprovedBy { get; set; }
+        public DateTime ApprovedAt { get; set; }
+        public string ModifiedBy { get; set; }
+        public string ModifiedAt { get; set; }
+		public string CounterPart { get; set; }
+        public string Area { get; set; }
+        public string AssetType { get; set; }
+        public string TechnologyType { get; set; }
+        public string CurrentState { get; set; }
+    }
+
+    //public class testing
+    //{
+    //    public void test()
+    //    {
+    //        Asset firas = new Asset();
+    //        firas.currentState = State.Draft;
+    //    }
+    //}
+}
+
+
+
