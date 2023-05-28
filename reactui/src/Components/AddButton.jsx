0fbb30b4c3e515e6ce1969a9ac950d8bdd86b561
firas-alalo <105157485+firas-alalo@users.noreply.github.com>
2023-05-28 19:04:17 +0200

    Create buttons components

---
+++ reactui/src/Components/AddButton.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+
+function AddButton() {
+    return (
+        <Box sx={{ '& > :not(style)': { m: 1 } }}>
+            <Fab color="primary" aria-label="add">
+                <AddIcon />
+            </Fab>
+        </Box>
+    );
+}
+
+export default AddButton;
