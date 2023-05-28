d25d7186c7d84a8b0d767d78d097321d87ce3273
firas-alalo <105157485+firas-alalo@users.noreply.github.com>
2023-05-28 19:12:41 +0200

    Add pages for the web app

---
+++ reactui/src/Pages/AssetDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AssetGrid from '../Components/AssetGrid';
+
+const AssetDetails = () => {
+    return (
+        <div>
+            <h1 className="titles">Add a new asset</h1>
+            <AssetGrid />
+        </div>
+    );
+}
+
+export default AssetDetails;
+
