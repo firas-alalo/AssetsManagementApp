7d1a89ffd2a7bb2df95e35089bb6b2466fb0469d
firas-alalo <105157485+firas-alalo@users.noreply.github.com>
2023-05-28 18:55:50 +0200

    Create connection to the database

---
+++ DataAccessLayer/DataAccess/DBConnection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace DataAccessLayer.DataAccess
+{
+	public class DBConnection
+	{
+		public static SqlConnectionStringBuilder _connectionStringBuilder = new()
+		{
+			DataSource = "hildur.ucn.dk",
+			InitialCatalog = "DMA-CSD-S212_10201984",
+			UserID = "DMA-CSD-S212_10201984",
+			Password = "Password1!",
+			Encrypt = false
+		};
+	}
+}
