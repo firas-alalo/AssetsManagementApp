0943048f03ddfdf375db44024284539cf70b14db
firas-alalo <105157485+firas-alalo@users.noreply.github.com>
2023-05-28 19:12:41 +0200

    Add pages for the web app

---
+++ reactui/src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomeGrid from '../Components/HomeGrid';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="titles">Assets</h1>
+            <HomeGrid />
+        </div> 
+
+    );
+}
+
+export default Home;
+
