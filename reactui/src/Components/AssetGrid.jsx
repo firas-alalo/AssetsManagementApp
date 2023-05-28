5b107bea326d0de3341536004b922f9aeca3dc5d
firas-alalo <105157485+firas-alalo@users.noreply.github.com>
2023-05-28 19:06:21 +0200

    Create layout for the webpages

---
+++ reactui/src/Components/AssetGrid.jsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import AssetForm from '../Components/AssetForm';
+
+function AssetGrid() {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+    return (
+        <Grid container direction={isMobile ? 'column' : 'row'} alignItems="center" justifyContent="center" sx={{ gap: 3 }}>
+            <Grid item sx={{ maxWidth: '1300px' }}>
+                <AssetForm />
+            </Grid>
+        </Grid>
+    );
+}
+
+export default AssetGrid;
