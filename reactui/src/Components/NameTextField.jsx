36a6acd88faa34d306ef7b08236e454d3aaaf41b
firas-alalo <105157485+firas-alalo@users.noreply.github.com>
2023-05-30 15:06:39 +0200

    Make Asset name input field accept user input

--- reactui/src/Components/NameTextField.jsx
+++ reactui/src/Components/NameTextField.jsx
@@ -1,8 +1,10 @@
-import * as React from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
-function NameTextField() {
+function NameTextField(props) {
+    const { type, name, label, placeholder, value, onChange } = props;
+
     return (
         <Box
             component="form"
@@ -12,7 +14,16 @@ function NameTextField() {
             noValidate
             autoComplete="off"
         >
-            <TextField style={{ width: '100%', backgroundColor: '#FFFFFF' }} id="outlined-basic" label="Name" variant="outlined" />
+            <TextField
+                style={{ width: '100%', backgroundColor: '#FFFFFF' }}
+                variant="outlined"
+                type={type}
+                name={name}
+                label={label}
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+            />
         </Box>
     );
 }
