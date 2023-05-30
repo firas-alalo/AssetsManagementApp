4b54ee1d2ac7fe02f6816ce0724acb45dc657df6
firas-alalo <105157485+firas-alalo@users.noreply.github.com>
2023-05-30 15:06:15 +0200

    Add functionality to date pickers to accept user input

--- reactui/src/Components/DatePicker.jsx
+++ reactui/src/Components/DatePicker.jsx
@@ -1,40 +1,52 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import dayjs from 'dayjs';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Box from '@mui/material/Box';
 
-export function StartDatePicker() {
-    const [value, setValue] = React.useState(dayjs());
+export function StartDatePicker({ contractStart, onChange }) {
+    const [value, setValue] = useState(contractStart ? dayjs(contractStart).format('YYYY-MM-DD') : '');
+
+    const handleDateChange = (newValue) => {
+        const formattedDate = newValue ? newValue.format('YYYY-MM-DD') : '';
+        setValue(formattedDate);
+        onChange({ target: { name: 'contractStart', value: formattedDate } });
+    };
 
     return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer components={['DatePicker', 'DatePicker']} sx={{ marginTop: '3%' }}>
+        <Box sx={{ mx: 'auto', maxWidth: 'fit-content', marginTop: "30px"}}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DatePicker
                     label="Start Date"
                     value={value}
-                    onChange={(newValue) => setValue(newValue)}
+                    onChange={handleDateChange}
                     format="DD-MM-YYYY"
                 />
-            </DemoContainer>
-        </LocalizationProvider>
+            </LocalizationProvider>
+        </Box>
     );
 }
 
-export function EndDatePicker() {
-    const [value, setValue] = React.useState(dayjs());
+export function EndDatePicker({ contractEnd, onChange }) {
+    const [value, setValue] = useState(contractEnd ? dayjs(contractEnd).format('YYYY-MM-DD') : '');
+
+    const handleDateChange = (newValue) => {
+        const formattedDate = newValue ? newValue.format('YYYY-MM-DD') : '';
+        setValue(formattedDate);
+        onChange({ target: { name: 'contractEnd', value: formattedDate } });
+    };
 
     return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer components={['DatePicker', 'DatePicker']} sx={{ marginTop: '3%' }}>
+        <Box sx={{ mx: 'auto', maxWidth: 'fit-content', marginTop: "30px" }}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DatePicker
                     label="End Date"
                     value={value}
-                    onChange={(newValue) => setValue(newValue)}
+                    onChange={handleDateChange}
                     format="DD-MM-YYYY"
                 />
-            </DemoContainer>
-        </LocalizationProvider>
+            </LocalizationProvider>
+        </Box>
     );
 }
