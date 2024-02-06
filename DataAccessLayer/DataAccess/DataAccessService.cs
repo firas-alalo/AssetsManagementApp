7060177dbf0a0ddbb432178c5426790c3d68330c
Firas Alalo <Firas.vfx@gmail.com>
2024-02-06 14:37:18 +0100

    Configure the data access service

---
+++ DataAccessLayer/DataAccess/DataAccessService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccessLayer.DataAccess
+{
+    public static class DataAccessService
+    {
+        public static IServiceCollection AddDataAccessServices(this IServiceCollection services)
+        {
+            services.AddScoped<DataContext, DataContext>();
+            return services;
+        }
+    }
+}
