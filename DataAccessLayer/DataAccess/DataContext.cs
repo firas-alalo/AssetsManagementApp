02a7008d189d13e949707061a1ad5f5903216483
Firas Alalo <Firas.vfx@gmail.com>
2024-02-06 14:37:18 +0100

    Configure the data access service

---
+++ DataAccessLayer/DataAccess/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccessLayer.DataAccess
+{
+    public class DataContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        public DataContext(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _connectionString = _configuration.GetConnectionString("MyConnectionString");
+
+            if (string.IsNullOrEmpty(_connectionString))
+            {
+                throw new InvalidOperationException("Connection string cannot be null.");
+            }
+        }
+        public SqlConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
