54cfa367d079c4bd40d483fb988d6063411349a1
Firas Alalo <Firas.vfx@gmail.com>
2024-02-06 14:41:54 +0100

    Connect to DB via DataContext class

---
+++ DataAccessLayer/DataAccess/Interfaces/IAssetsDAO.cs
@@ -0,0 +1,15 @@
+﻿using DomainModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DataAccess.Interfaces
+{
+    public interface IAssetsDAO
+    {
+        List<Asset> GetAssets();
+        bool AddAsset(Asset asset);
+	}
+}
