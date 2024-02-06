e20b12e9c550acb80b7672641f0030c2d4f83eb0
Firas Alalo <Firas.vfx@gmail.com>
2024-02-06 15:02:43 +0100

    Add basic navbar with routing

--- reactui/src/App.js
+++ reactui/src/App.js
@@ -2,16 +2,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import './App.css';
 import AssetDetails from './Pages/AssetDetails';
+import Navbar from './Components/Navbar';
 
 const App = () => {
+
     return (
-        <BrowserRouter>
-            {/*<Navigation />*/}
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/asset" element={<AssetDetails />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/asset" element={<AssetDetails />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+
     );
 }
 
