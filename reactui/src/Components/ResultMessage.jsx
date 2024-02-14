4674350c044b2ba9d7c77bddbdf6cb1fad6e676c
Firas Alalo <Firas.vfx@gmail.com>
2024-02-14 03:43:49 +0100

    Add Return to Homepage to Result messages component

--- reactui/src/Components/ResultMessage.jsx
+++ reactui/src/Components/ResultMessage.jsx
@@ -1,23 +1,27 @@
 import * as React from 'react';
 import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 export function SuccessMessage() {
     return (
-        <Stack sx={{ width: '50%' }} spacing={2}>
+        <Stack sx={{ width: '42%' }} spacing={1}>
             <Alert variant="filled" severity="success">
                 A new asset has been created!
             </Alert>
+            <Button variant="contained" href="/">Return to Homepage</Button>
+
         </Stack>
     );
 }
 
 export function FailedMessage() {
     return (
-        <Stack sx={{ width: '50%' }} spacing={2}>
+        <Stack sx={{ width: '42%' }} spacing={1}>
             <Alert variant="filled" severity="error">
                 An error occured while creating a new asset!
             </Alert>
+            <Button variant="contained" href="/">Return to Homepage</Button>
         </Stack>
     );
 }
