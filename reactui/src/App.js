7870c667a9f298afe69d26e2ac27a7fd69bf0314
Firas Alalo <Firas.vfx@gmail.com>
2024-03-23 21:25:35 +0100

    Remove unused components and code

--- reactui/src/App.js
+++ reactui/src/App.js
@@ -1,23 +1,17 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home';
-import './App.css';
-import AssetDetails from './Pages/AssetDetails';
-import Navbar from './Components/Navbar';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import "./App.css";
 
 const App = () => {
-
-    return (
-        <>
-            <BrowserRouter>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/asset" element={<AssetDetails />} />
-                </Routes>
-            </BrowserRouter>
-        </>
-
-    );
-}
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
